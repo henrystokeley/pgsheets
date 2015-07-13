97f7a406d53794e6e9ee7f56d64d53edad3c9fd6
Henry Stokeley <hstokeley@gmail.com>
2015-07-14 00:06:15 +0900

    add classifiers and keywords to setup.py
    As described in:
    https://packaging.python.org/en/latest/distributing.html#setup-args

--- setup.py
+++ setup.py
@@ -29,4 +29,12 @@ if __name__ == '__main__':
           url="https://github.com/henrystokeley/pgsheets",
           install_requires=requirements,
           test_suite='test',
+          classifiers=[
+              'Development Status :: 3 - Alpha',
+              'License :: OSI Approved :: MIT License',
+              'Programming Language :: Python :: 3.4',
+              'Topic :: Scientific/Engineering',
+              'Topic :: Office/Business :: Financial :: Spreadsheet',
+              ],
+          keywords='pandas google sheets spreadsheets dataframe',
           )
