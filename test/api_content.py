097b7803eb16354c99dbdda25297fb48096a6a5c
Henry Stokeley <hstokeley@gmail.com>
2015-07-18 18:01:08 +0900

    add tests for Spreadsheet class

---
+++ test/api_content.py
@@ -0,0 +1,135 @@
+"""Responses from Google API for use in testing"""
+import datetime
+
+
+def get_spreadsheet_element(key="test_key", title="title"):
+    d = datetime.datetime(2015, 7, 18)
+    data = (
+        '<ns0:entry xmlns:ns0="http://www.w3.org/2005/Atom">'
+
+        '<ns0:id>https://spreadsheets.google.com/feeds/spreadsheets/'
+        'private/full/{key}</ns0:id>'
+
+        '<ns0:updated>{update_year}-{update_month:02d}-{update_day:02d}'
+        'T05:29:31.140Z</ns0:updated>'
+
+        '<ns0:category scheme="http://schemas.google.com/spreadsheets/'
+        '2006" term="http://schemas.google.com/spreadsheets/'
+        '2006#spreadsheet" />'
+
+        '<ns0:title type="text">{title}</ns0:title>'
+
+        '<ns0:content type="text">{title}</ns0:content>'
+
+        '<ns0:link href="https://spreadsheets.google.com/feeds/worksheets/'
+        '{key}/private/full" rel="http://schemas.google.com/spreadsheets/'
+        '2006#worksheetsfeed" type="application/atom+xml" />'
+
+        '<ns0:link href="https://docs.google.com/spreadsheets/d/{key}/'
+        'edit" rel="alternate" type="text/html" />'
+
+        '<ns0:link href="https://spreadsheets.google.com/feeds/'
+        'spreadsheets/private/full/{key}" rel="self" '
+        'type="application/atom+xml" />'
+
+        '<ns0:author>'
+        '<ns0:name>{username}</ns0:name>'
+        '<ns0:email>{mail}</ns0:email>'
+        '</ns0:author>'
+        '</ns0:entry>'
+        .format(key=key,
+                mail="fake@example.com",
+                username="fake",
+                update_year=d.year,
+                update_month=d.month,
+                update_day=d.day,
+                title=title))
+    return data.encode()
+
+
+def get_worksheets_feed(key, sheet_names=["title"]):
+    def get_entry(sheet_title):
+        return (
+            "<entry>"
+            "<id>https://spreadsheets.google.com/feeds/worksheets/{key}/"
+            "private/full/{id}</id>"
+            "<updated>2015-07-18T05:29:31.112Z</updated>"
+            "<category scheme='http://schemas.google.com/spreadsheets/"
+            "2006' term='http://schemas.google.com/spreadsheets/2006#"
+            "worksheet'/>"
+            "<title type='text'>{sheet_title}</title>"
+            "<content type='text'>{sheet_title}</content>"
+
+            "<link rel='http://schemas.google.com/spreadsheets/2006#"
+            "listfeed' type='application/atom+xml' href='https://spreadshe"
+            "ets.google.com/feeds/list/{key}/{id}/private/full'/>"
+            "<link rel='http://schemas.google.com/spreadsheets/2006#"
+            "cellsfeed' type='application/atom+xml' href='https://"
+            "spreadsheets.google.com/feeds/cells/{key}/{id}/private/full'/>"
+            "<link rel='http://schemas.google.com/visualization/2008#"
+            "visualizationApi' type='application/atom+xml' href='"
+            "https://docs.google.com/spreadsheets/d/{key}/gviz/tq?gid=0'/>"
+            "<link rel='http://schemas.google.com/spreadsheets/2006#"
+            "exportcsv' type='text/csv' href='https://docs.google.com/"
+            "spreadsheets/d/{key}/export?gid=0&amp;format=csv'/>"
+            "<link rel='self' type='application/atom+xml' href='https://"
+            "spreadsheets.google.com/feeds/worksheets/{key}/private/full/"
+            "{id}'/>"
+            "<link rel='edit' type='application/atom+xml' href='"
+            "https://spreadsheets.google.com/feeds/worksheets/{key}/"
+            "private/full/{id}/{version}'/>"
+            "<gs:colCount>{col_count}</gs:colCount>"
+            "<gs:rowCount>{row_count}</gs:rowCount>"
+            "</entry>"
+            .format(key=key, col_count=2, row_count=2,
+                    sheet_title=sheet_title, id="od6", version="CCCC")
+            )
+    entries = "".join(get_entry(t) for t in sheet_names)
+
+    d = datetime.datetime(2015, 7, 18)
+    data = (
+        "<?xml version='1.0' encoding='UTF-8'?>"
+        "<feed xmlns='http://www.w3.org/2005/Atom'"
+        " xmlns:openSearch='http://a9.com/-/spec/opensearchrss/1.0/'"
+        " xmlns:gs='http://schemas.google.com/spreadsheets/2006'>"
+
+        "<id>https://spreadsheets.google.com/feeds/worksheets/{key}/"
+        "private/full</id>"
+        '<updated>{update_year}-{update_month:02d}-{update_day:02d}'
+        'T05:29:31.140Z</updated>'
+        "<category scheme='http://schemas.google.com/spreadsheets/2006' "
+        "term='http://schemas.google.com/spreadsheets/2006#worksheet'/>"
+        "<title type='text'>{title}</title>"
+
+        "<link rel='alternate' type='application/atom+xml' href='"
+        "https://docs.google.com/spreadsheets/d/{key}/edit'/>"
+        "<link rel='http://schemas.google.com/g/2005#feed' type="
+        "'application/atom+xml' href='https://spreadsheets.google.com/"
+        "feeds/worksheets/{key}/private/full'/>"
+        "<link rel='http://schemas.google.com/g/2005#post' type='"
+        "application/atom+xml' href='https://spreadsheets.google.com/feeds"
+        "/worksheets/{key}/private/full'/>"
+        "<link rel='self' type='application/atom+xml' href='https://"
+        "spreadsheets.google.com/feeds/worksheets/{key}/private/full'/>"
+
+        "<author>"
+        "<name>{username}</name>"
+        "<email>{mail}</email>"
+        "</author>"
+
+        "<openSearch:totalResults>{results}</openSearch:totalResults>"
+        "<openSearch:startIndex>1</openSearch:startIndex>"
+
+        "{entries}"
+
+        "</feed>"
+        .format(key=key,
+                mail="fake@example.com",
+                username="fake",
+                update_year=d.year,
+                update_month=d.month,
+                update_day=d.day,
+                title="title",
+                entries=entries,
+                results=len(sheet_names)))
+    return data.encode()
---
+++ test/test_models.py
@@ -0,0 +1,155 @@
+from unittest import TestCase
+from unittest.mock import patch
+
+from pgsheets import Spreadsheet
+from pgsheets.models import Worksheet
+from pgsheets.exceptions import PGSheetsHTTPException
+
+from test.api_content import get_spreadsheet_element, \
+    get_worksheets_feed
+
+
+class MockToken():
+    """Mocks a Token class.
+
+    The authorization header it returns is completely fake and
+    non-changing.
+    """
+    token = "non_changing_token"
+
+    def getAuthorizationHeader(self, headers=None):
+        if headers is None:
+            headers = {}
+        headers['Authorization'] = "Bearer {}".format(self.token)
+        return headers
+
+
+class ApiTest(TestCase):
+    """Useful methods and setup for get and post calls
+    """
+
+    def setUp(self):
+        self.get_patch = patch("requests.get")
+        self.get = self.get_patch.start()
+        self.post_patch = patch("requests.post")
+        self.post = self.post_patch.start()
+
+        self.token = MockToken()
+
+    def tearDown(self):
+        self.post_patch.stop()
+        self.get_patch.stop()
+
+    def checkGetCall(self, url=None):
+        """Test that a Get request was made with appropriate
+        authorization headers.
+
+        Optionally can check the url. Resets the called attribute
+        """
+        self.assertTrue(self.get.called)
+        pos, kwargs = self.get.call_args
+        assert len(pos) >= 1
+        if url is not None:
+            self.assertEqual(pos[0], url)
+        self.assertIn('headers', kwargs)
+        self.assertIn('Authorization', kwargs['headers'])
+        self.assertEqual(kwargs['headers']['Authorization'],
+                         "Bearer " + self.token.token)
+        self.get.called = False
+
+
+class TestSpreadSheet(ApiTest):
+
+    def test_initialize(self):
+        # we should be able to intialize a Spreadsheet with a variety
+        # of different URLs
+        key = "TESTKEY"
+        test_keys = [
+            key,  # just using key
+            "https://docs.google.com/spreadsheets/d/{}/edit#gid=0"
+            .format(key),  # normal url
+            "http://docs.google.com/spreadsheets/d/{}/edit#gid=0"
+            .format(key),  # not https
+            "https://docs.google.com/spreadsheets/d/{}"
+            .format(key),  # no trailing slash
+            "https://www.docs.google.com/spreadsheets/d/{}"
+            .format(key),  # includes www
+            "www.docs.google.com/spreadsheets/d/{}"
+            .format(key),  # includes www, no http
+            "docs.google.com/spreadsheets/d/{}/"
+            .format(key),  # no http
+            ]
+
+        # check that we call the right URL
+        feed_url = ("https://spreadsheets.google.com/feeds/spreadsheets/"
+                    "private/full/{}")
+
+        self.get.return_value.status_code = 200
+        self.get.return_value.content = get_spreadsheet_element(key=key)
+        for k in test_keys:
+            s = Spreadsheet(self.token, key)
+            self.checkGetCall(url=feed_url.format(key))
+            self.assertEqual(s.getKey(), key)
+
+        # a bad key gets an error
+        self.get.return_value.status_code = 500
+        bad_key = "BAD_KEY"
+        with self.assertRaises(PGSheetsHTTPException):
+            Spreadsheet(self.token, bad_key)
+        self.checkGetCall(url=feed_url.format(bad_key))
+
+        self.assertFalse(self.post.called)
+
+    def getSpreadsheet(self, key="TESTKEY", title="my_title"):
+        """Helper method to get a Spreadsheet object by faking an API
+        response.
+        """
+        self.get.return_value.status_code = 200
+        self.get.return_value.content = (
+            get_spreadsheet_element(key=key, title="my_title"))
+        return Spreadsheet(self.token, key)
+
+    def test_getKey_and_getTitle(self):
+        s = self.getSpreadsheet("TESTKEY", "my_title")
+        self.assertEqual(s.getTitle(), "my_title")
+        self.assertEqual(s.getKey(), "TESTKEY")
+
+    def test_repr(self):
+        s = self.getSpreadsheet("TESTKEY", "my_title")
+        self.assertEqual(repr(s),
+                         "<Spreadsheet title='my_title' key='TESTKEY'>")
+
+    def test_getURL(self):
+        s = self.getSpreadsheet("TESTKEY", "my_title")
+        self.assertEqual(s.getURL(),
+                         "https://docs.google.com/spreadsheets/d/{}/edit"
+                         .format("TESTKEY"))
+
+    def test_getWorksheets(self):
+        key = "TESTKEY"
+        s = self.getSpreadsheet(key, "my_title")
+
+        # get spreadsheets
+        self.get.return_value.status_code = 200
+        self.get.return_value.content = get_worksheets_feed(
+            key=key, sheet_names=["sheet_title"])
+        w = s.getWorksheets()
+        self.checkGetCall(
+            "https://spreadsheets.google.com/feeds/worksheets/{}/private/full"
+            .format(key))
+
+        self.assertEqual(type(w), list)
+        self.assertEqual(len(w), 1)
+        self.assertEqual(type(w[0]), Worksheet)
+        w = w[0]
+
+        self.assertEqual(w._getTitle(w._element), "sheet_title")
+        self.assertEqual(w._getSheetKey(), s.getKey())
+
+        w = s.getWorksheet('sheet_title')
+        self.assertEqual(type(w), Worksheet)
+        self.assertEqual(w._getTitle(w._element), "sheet_title")
+        self.assertEqual(w._getSheetKey(), s.getKey())
+
+        with self.assertRaises(ValueError):
+            s.getWorksheet("fake worksheet")
